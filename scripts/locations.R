050ac826cea1700b8519c539a0f3db50379aeb3f
Siddharth Reed <sid.reed@icahn.mssm.edu>
2026-09-13 19:53:28 -0400

    re-write to use here() to define relative filepaths

--- scripts/locations.R
+++ scripts/locations.R
@@ -2,24 +2,16 @@
 # Relevant directories
 #############################
 # PROJECT_DIR <- "/sc/arion/projects/Microglia/CRD.Analysis.Sid.Rotation"
-MICROGLIA_DIR <- "/sc/arion/projects/Microglia"
 RESIDUALS_DIR <- 
-    file.path(
-        MICROGLIA_DIR,
+    here(
+        '..',
         "Combined_RNAseq_ATACseq",
         "ATACseq_analyses",
-        "Data_processing", 
+        "Data_processing",
         "files"
     )
-SAMPLE_METADATA_RDS_FILEPATH <- 
-    file.path(
-        RESIDUALS_DIR,
-        "AllInfo_ATACseq_processing_209x362.RDs"
-    )
-
-SAMPLE_METADATA_TSV_FILEPATH <- 
-    file.path(
-        PROJECT_DIR,
-        "all.sample.metadata.tsv"
-    )
-
+SAMPLE_METADATA_RDS_FILEPATH <- file.path(RESIDUALS_DIR, "AllInfo_ATACseq_processing_209x362.RDs")
+SAMPLE_METADATA_TSV_FILEPATH <- here("all.sample.metadata.tsv")
+DECORATE_RESULTS_DIR         <- here("results", "decorate")
+SVA_RESULTS_DIR              <- here("results", "peak.SVs")
+CRD_RESULTS_DIR              <- here("results", "decorate.CRDs")
