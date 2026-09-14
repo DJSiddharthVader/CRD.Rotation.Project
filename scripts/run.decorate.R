c43737a4bd8ba02f678b87c4781a9391a10b8fd9
Siddharth Reed <sid.reed@icahn.mssm.edu>
2026-09-13 20:05:17 -0400

    list all combinations of relevant input data and hyper-parameters for CRD calling and comparison

--- scripts/run.decorate.R
+++ scripts/run.decorate.R
@@ -8,6 +8,49 @@ source(here('scripts', 'utils.decorate.R'))
 # load patient metadata
 sample.metadata <- load_sample_metadata()
 
+############################################################
+# Generate all input dataset + hyper-param combinations to test
+############################################################
+all.input.and.parameter.combinations.df <- 
+    # First list residual ATAC-seq count matrices that can be used to annotate CRDs
+    list_all_ATAC_residual_sets() %>% 
+    # get data specifying peak coordinates for each set of residuals
+    left_join(
+        list_all_ATAC_coordinates_sets(),
+        by=join_by(CPM.cutoff)
+    ) %>% 
+    filter(CPM.cutoff == '3CPM') %>% 
+    filter(residual.model == 'KeepDxAge') %>% 
+    # decorate hyper-parameter values to test
+    cross_join(
+        tibble(
+            # Different inputs to generate CRD annotations from
+            sample.strategy=
+                c(
+                    'Control.Samples',   # only annotated CRDs using Control samples
+                    'AD.Samples',        # only annotate CRDs using AD samples
+                    'All.Samples'        # annotate CRDs using All samples
+                ),
+            # which metadata column we use to define sample conditions
+            AD.definition.column=
+                c(
+                    # 'Braak_3levels',   # Braak_5plus, Braak_3to4, Braak_Max2
+                    'AD_CERAD_withDLB' # AD, Control, Other
+                )
+        )
+    ) %>% 
+    cross_join(
+        tibble(
+            adjacentCount=c(500),
+            method.corr=c("spearman"),
+            clusterMethod=c("meanClusterSize"),
+            # meanClusterSize=list(list(20, 30, 40, 50)),
+            meanClusterSize=list(list(10, 25, 50, 80, 100)),
+            filterMetric=c("LEF"),
+            filterMetricCutoff=c(0.15),
+            jaccardCutoff=c(0.9)
+        )
+    )
 
 ############################################################
 # Compute Surrogate Variables (SVs) and produce corrected matrices
